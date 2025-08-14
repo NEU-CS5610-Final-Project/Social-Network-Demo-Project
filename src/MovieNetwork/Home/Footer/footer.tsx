6a2702563f3ce1e1b46c74b072a8ec6895d49d26
fcges <fengrunyuan1234567@126.com>
2025-08-13 23:25:44 -0700

    fix: added copyright information and github link to footer in home page

--- src/MovieNetwork/Home/Footer/footer.tsx
+++ src/MovieNetwork/Home/Footer/footer.tsx
@@ -1,25 +1,40 @@
+import { Button } from "react-bootstrap";
+
 export default function Footer() {
-    return (
-      <footer
-        style={{
-          backgroundColor: "#f8f9fa",
-          padding: "20px 0",
-          textAlign: "center",
-          fontSize: "0.9rem",
-          borderTop: "1px solid #ddd",
-          marginTop: "40px",
-        }}
-      >
-        <div style={{ marginBottom: "8px" }}>
-          <img
-            src="/TMDB_logo/TMDB.jpg"
-            alt="TMDB Logo"
-            style={{ height: "30px", marginRight: "8px", verticalAlign: "middle" }}
-          />
+  return (
+    <footer
+      style={{
+        backgroundColor: "#f8f9fa",
+        padding: "20px 0",
+        fontSize: "0.9rem",
+        borderTop: "1px solid #ddd",
+        marginTop: "40px",
+      }}
+    >
+      <div style={{ marginBottom: "8px" }} className="text-center">
+        <img
+          src="/TMDB_logo/TMDB.jpg"
+          alt="TMDB Logo"
+          style={{ height: "30px", marginRight: "8px", verticalAlign: "middle" }}
+        />
+      </div>
+      <p style={{ margin: 0, color: "#6c757d" }} className="text-center">
+        This product uses the TMDB API but is not endorsed or certified by TMDB.
+      </p>
+      <div className="ms-3 d-flex justify-content-between align-items-center">
+        <small style={{ margin: 0, color: "#6c757d" }}>
+          © {new Date().getFullYear()} Movify. All rights reserved. <br />
+          Development Team: Junyao HAN, Qiuzi WU, Jiaming PEI, Runyuan FENG
+        </small>
+        <div>
+          <Button variant="link" className="p-0">
+            <a href="https://github.com/NEU-CS5610-Final-Project/Social-Network-Demo-Project" style={{ color: "#6c757d" }} className="me-2"><small>React Project</small></a>
+          </Button>
+          <Button variant="link" className="p-0">
+            <a href="https://github.com/NEU-CS5610-Final-Project/Social-Network-Demo-Remote-Server" style={{ color: "#6c757d" }} className="me-2"><small>Server Project</small></a>
+          </Button>
         </div>
-        <p style={{ margin: 0, color: "#6c757d" }}>
-          This product uses the TMDB API but is not endorsed or certified by TMDB.
-        </p>
-      </footer>
-    );
-  }
+      </div>
+    </footer>
+  );
+}
